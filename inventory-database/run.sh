e8f6f96118ceb79135870fe08fd2490e80f6d40c
Gal <galbarda.dev@gmail.com>
2023-02-18 07:50:59 +0200

    transition to docker compose

--- inventory-database/run.sh
+++ inventory-database/run.sh
@@ -1,1 +1,1 @@
-docker run -dp 5432:5432 --name products-db --network-alias products-db --mount type=volume,src=products-db,target=/var/lib/postgresql/data products-db
+docker run -dp 5432:5432 --name products-db --network hayot-management --network-alias products-db --mount type=volume,src=products-db,target=/var/lib/postgresql/data products-db
