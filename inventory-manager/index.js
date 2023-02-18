4b09927a1bcfa7029966312ad66894b4cb3639fd
vboxuser <vboxuser@xubun2.myguest.virtualbox.org>
2023-02-17 18:34:41 -0700

    update network

--- inventory-manager/index.js
+++ inventory-manager/index.js
@@ -3,22 +3,25 @@ const app = express()
 const port = 5000
 
 const { Pool, Client } = require('pg')
- 
-// const client = new Client({
-//   user: process.env.POSTGRES_USER,
-//   host: process.env.POSTGRES_DB,
-//   database: process.env.POSTGRES_DB,
-//   password: process.env.POSTGRES_PASSWORD,
-//   port: process.env.POSTGRES_PORT,
-// })
 
-const client = new Client({
-  user: "postgres",
-  host: "postgres://postgres/db",
-  database: "products",
-  password: "docker",
-  port: "5432",
-})
+const connection = {
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+}
+
+const client = new Client(connection);
+
+console.log(JSON.stringify(client));
+// const client = new Client({
+//   user: "postgres",
+//   host: "postgres://postgres/db",
+//   database: "products",
+//   password: "docker",
+//   port: "5432",
+// })
 
 console.log("LALALA" + process.env.POSTGRES_HOST)
 
