af6b861686a137254ea675bbcfbe050fb91a224c
Gal <galbarda.dev@gmail.com>
2023-02-18 03:10:45 +0200

    update run and build

--- inventory-database/build.sh
+++ inventory-database/build.sh
@@ -1,1 +1,4 @@
-docker build -t inventory-database ./
+#create docker network if it does not exists
+docker network inspect hayot-management >/dev/null 2>&1 || \
+    docker network create --driver bridge hayot-management
+docker build -t products-db ./
