d59b9396ba732f2097990bf629c85050ffb3577e
Gal <galbarda.dev@gmail.com>
2023-02-23 11:34:26 +0200

    update index.js

--- inventory-manager/index.js
+++ inventory-manager/index.js
@@ -6,7 +6,38 @@ const db = require("./db/queries")
 
 app.get('/', async (req, res) => {
   try {
-    results = await db.query("SELECT * FROM brfands")
+    results = await db.query("SELECT * FROM brands")
+    res.send(results.rows)
+  } catch (error) {
+    console.error(`ERROR: ${error}`)
+    res.status(500).send("Error")
+  }
+})
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    results = await db.query("SELECT * FROM brands")
+    res.send(results.rows)
+  } catch (error) {
+    console.error(`ERROR: ${error}`)
+    res.status(500).send("Error")
+  }
+})
+
+app.get('/products/name/:name', async (req, res) => {
+  try {
+    console.log(req.params)
+    results = await db.query(`SELECT * FROM products WHERE name= '$1'`, [req.params.name])
+    res.send(results.rows)
+  } catch (error) {
+    console.error(`ERROR: ${error}`)
+    res.status(500).send("Error")
+  }
+})
+
+app.get('/products/:name', async (req, res) => {
+  try {
+    results = await db.query("SELECT * FROM brands")
     res.send(results.rows)
   } catch (error) {
     console.error(`ERROR: ${error}`)
