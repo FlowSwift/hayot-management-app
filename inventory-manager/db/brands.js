5283e15a6184ceeb45834b15d8ee4a354868b77b
FlowSwift <galbarda.dev@gmail.com>
2023-07-13 22:51:36 +0300

    add createBrand, updateBrandByID routes

--- inventory-manager/db/brands.js
+++ inventory-manager/db/brands.js
@@ -2,7 +2,8 @@
 const { query } = require("express");
 const db = require("./queries")
 const util = require("./util")
-const baseQuery = "SELECT id, name FROM brands"; // base query to append on
+const baseQuery = "SELECT id, name, COUNT(id) OVER () AS total_count FROM brands"; // base query to append on
+const orderBy = "brands.id ASC"
 
 /**
  * get a list of brands
@@ -11,7 +12,8 @@ const baseQuery = "SELECT id, name FROM brands"; // base query to append on
  * @returns Array of results or undefined upon error/ null if no brands were found
  */
 async function getBrands(limit, offset) {
-    let query = util.addPagination(baseQuery, limit, offset);
+    let query = baseQuery + " ORDER BY " + orderBy;
+    query = util.addPagination(query, limit, offset);
     let results = await db.queryDB(query);
     if (results === undefined) {
         return undefined
@@ -65,7 +67,7 @@ async function getBrandByName(name) {
  */
 async function getBrandsBySubstring(substring, limit, offset) {
     //add conditions to base query and match name to a substring with any prefix and suffix
-    let query = baseQuery + " WHERE brands.name iLIKE REPLACE('%?%', '?', $1)";
+    let query = baseQuery + " WHERE brands.name iLIKE REPLACE('%?%', '?', $1) ORDER BY " + orderBy;
     query = util.addPagination(query, limit, offset);
     //query
     let results = await db.queryDB(query, [substring]);
@@ -79,9 +81,57 @@ async function getBrandsBySubstring(substring, limit, offset) {
     return results.rows
 }
 
+/**
+ * create a brand.
+ * @param {object} brand - must include name, price, weight, ean, quantity and brand_id
+ * @returns - brand name and newly associated ID
+ */
+async function createBrand(brand) {
+    const {name, brand_id, animal_id} = brand
+    /*
+    exmaple query: "INSERT INTO brands(name, brand_id, animal_id) VALUES('Huskey la vista', 2, 2)"
+    */
+    if (util.isNameEmpty(name)) {
+        return undefined
+    }
+    let query = "INSERT INTO brands(name) VALUES($1) RETURNING name, id"
+    let results = await db.queryDB(query, [name])
+    //error handling
+    if (results === undefined) {
+        return undefined
+    }
+    if (results.rowCount == 0) {
+        return null
+    }
+    return results.rows
+}
+
+/**
+ * update a brand by ID
+ * @param {object} brand - brand object - valid brand id required and one or more of the existing brand properties to update
+ * @returns the id and name of a brand from a given id or undefined upon error/null if no brand exists with the given id
+ */
+async function updateBrandByID (brand) {
+    //set base query
+    let query = "UPDATE brands SET";
+    let values = [];
+    [query, values] = util.updateItem(query, brand);
+    let results = await db.queryDB(query, values);
+    //error handling
+    if (results === undefined) {
+        return undefined
+    }
+    if (results.rowCount == 0) {
+        return null
+    }
+    return results.rows
+}
+
 module.exports = {
     getBrands,
     getBrandByID,
     getBrandByName,
-    getBrandsBySubstring
+    getBrandsBySubstring,
+    createBrand,
+    updateBrandByID
 }
--- inventory-manager/routes/brands.js
+++ inventory-manager/routes/brands.js
@@ -18,5 +18,66 @@ router.get('/brands', async (req, res) => {
     return res.send(results)
 })
 
+/**
+ * fetch brands by name
+ * use strict query paramater for substring search //false by default
+ * add limit and offset url queries params for pagination otherwise default would apply
+ */
+router.get('/brands/name/:name', async (req, res) => {
+    // handle strict flag
+    let strict = false
+    let queryStrict = req.query.strict
+    if (queryStrict !== undefined) {
+        switch (queryStrict) {
+            case "true":
+                strict = true
+                break;
+            case "false":
+                break;
+            default:
+                return res.status(400).send("illegal strict value")
+        }
+    }
+    // request handling
+    let results
+    if (strict) {
+        results = await brandsQuery.getBrandByName(req.params.name)
+    }
+    else {
+        results = await brandsQuery.getBrandsBySubstring(req.params.name, req.query.limit, req.query.offset)
+    }
+    //response handling
+    util.setStatus(res, results)
+    if (res.statusCode != 200) {
+        return util.sendStatusMessage(res)
+    }
+    return res.send(results)
+})
+
+// Create new brand
+router.post('/brands', async (req, res) => {
+    // request handling
+    let results = await brandsQuery.createBrand(req.body)
+    //response handling
+    util.setStatus(res, results)
+    if (res.statusCode != 200) {
+        return util.sendStatusMessage(res)
+    }
+    return res.send(results)
+})
+
+module.exports = router;
+
+// update brands by id
+router.put('/brands', async (req, res) => {
+    // request handling
+    let results = await brandsQuery.updateBrandByID(req.body)
+    //response handling
+    util.setStatus(res, results)
+    if (res.statusCode != 200) {
+        return util.sendStatusMessage(res)
+    }
+    return res.send(results)
+})
 
 module.exports = router;
