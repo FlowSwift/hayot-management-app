60bf0fe7b93086c41520f728d71c1ec3650f5d28
FlowSwift <galbarda.dev@gmail.com>
2023-07-13 06:26:18 +0300

    add product quantity input field

--- client/src/products/ProductTableRow.tsx
+++ client/src/products/ProductTableRow.tsx
@@ -1,12 +1,14 @@
 import { FC, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { Product, Category } from "../common/types";
+import { Col, Form, Row } from 'react-bootstrap';
 
 interface Props {
     product: Product,
     handleEditProduct: Function
 };
 const ProductTableRow: FC<Props> = ({ product, handleEditProduct }) => {
+    const [quantity, setQuantity] = useState(0);
     const handleOpen = () => handleEditProduct(product);
 
     return (
@@ -17,6 +19,25 @@ const ProductTableRow: FC<Props> = ({ product, handleEditProduct }) => {
             </td>
             <td className="align-middle">{product.brand_name}</td>
             <td className="align-middle">
+                <Form>
+                    <Row className="align-items-center">
+                        <Col xs="auto">
+                            <Form.Control
+                                className="small-input"
+                                value={quantity}
+                                type="number"
+                                min="0"
+                                step="1"
+                                onChange={(e) => setQuantity(parseFloat(e.target.value))}
+                            />
+                        </Col>
+                        <Col xs="auto">
+                            <Button variant="outline-primary" size="sm">
+                                ✓
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
             </td>
             <td className="align-middle">{product.ean}</td>
             <td className="align-middle">{product.category_name}</td>
