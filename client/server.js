6ed66e2798288bcc56a4ed466ddbde5c955314b2
FlowSwift <galbarda.dev@gmail.com>
2023-08-02 01:41:54 +0300

    prod setup

--- client/server.js
+++ client/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -8,4 +10,16 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+const httpsOptions = {
+  cert: fs.readFileSync('/etc/letsencrypt/live/manage.prins-israel.co.il/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/manage.prins-israel.co.il/privkey.pem')
+};
+
+// Create the HTTPS server
+const server = https.createServer(httpsOptions, app);
+
+// Start the server on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
