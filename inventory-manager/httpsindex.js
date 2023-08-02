85d77311b67000f74ee999548c03c169f7090245
FlowSwift <galbarda.dev@gmail.com>
2023-08-02 05:11:08 +0300

    prod startup for backend

---
+++ inventory-manager/httpsindex.js
@@ -0,0 +1,66 @@
+"use strict";
+const express = require('express')
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
+const app = express();
+const port = 5000;
+
+const productsRouter = require("./routes/products")
+const brandsRouter = require("./routes/brands")
+const categoriesRouter = require("./routes/categories")
+const animalsRouter = require("./routes/animals")
+const logsRouter = require("./routes/logs")
+const authRouter = require("./routes/auth.js")
+const authMiddleware = require("./auth/authMiddleware.js")
+
+const db = require("./db/queries")
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/manage.prins-israel.co.il/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/manage.prins-israel.co.il/fullchain.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+// ensure any page can access the API resources
+app.use(cors({
+  origin: '*'
+}));
+
+// delay all routes, use for testing purposes
+// app.use((req, res, next) => {
+//   const delay = 5000;
+
+//   console.log(`Requested route: ${req.method} ${req.originalUrl}`);
+//   console.log('Pausing for 2 seconds...');
+
+//   setTimeout(() => {
+//     console.log('Resuming execution.');
+//     next();
+//   }, delay);
+// });
+app.use(express.json())
+app.use("/products", authMiddleware)
+app.use(productsRouter)
+app.use("/brands", authMiddleware)
+app.use(brandsRouter)
+app.use("/categories", authMiddleware)
+app.use(categoriesRouter)
+app.use("/animals", authMiddleware)
+app.use(animalsRouter)
+app.use("/logs", authMiddleware)
+app.use(logsRouter)
+app.use(authRouter)
+
+app.get('/', async (req, res) => {
+  try {
+    res.send("Index")
+  } catch (error) {
+    console.error(`ERROR: ${error}`)
+    res.status(500).send("Error")
+  }
+})
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+  console.log(`Example app listening on port ${port} with HTTPS`);
+});
