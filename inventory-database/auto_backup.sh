004de7a3e21dd8b7d62a83ebac35706383b75f09
FlowSwift <galbarda.dev@gmail.com>
2023-08-02 06:10:15 +0300

    fix backup path

--- inventory-database/auto_backup.sh
+++ inventory-database/auto_backup.sh
@@ -1,7 +1,7 @@
 # Define variables
 POSTGRES_CONTAINER="inventory-database"
 USERNAME="postgres"
-BACKUP_DIR="db_backup"
+BACKUP_DIR="/root/repos/hayot-management-app/inventory-database/db_backup"
 BACKUP_FILE="$BACKUP_DIR/backup_$(date +%Y-%m-%d_%H-%M).sql"
 
 # Create the backup directory if it doesn't exist
